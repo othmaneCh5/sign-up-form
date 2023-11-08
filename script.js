3d8125897a7396e5e9d26ca607c2f8ca3696684a
othmaneCh5 <wiltouzehy@gmail.com>
2023-11-08 14:38:32 +0100

    i finnished the sign up page i changed the entire design

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //selecting elements
-const resetButton = document.getElementById('resetButton');
 const form=document.getElementById('form');
-const submitbutton=document.getElementById('submitbutton');
 const confirm_password=document.getElementById('confirm_password');
 const password=document.getElementById('password');
 //making a function to check that the password and the confirmation are the same
@@ -12,7 +10,4 @@ form.addEventListener('submit', function (event) {
       event.preventDefault(); // Prevent form submission
     }
   });
-//adding event to the reset button so it can rest the content of the form
-resetButton.addEventListener('click', function () {
-   form.reset();
-  });
+
